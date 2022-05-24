7567e3566fec543a4b40ccf395ccb6ed5374f6f4
fabrizioiacobucci <fabrizio.iacobucci90@mail.com>
2022-05-24 20:10:05 +0200

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+    upload: {
+        breakpoints: {
+            xlarge: 1920,
+            large: 1000,
+            medium: 750,
+            small: 500,
+            xsmall: 64
+        }
+    }
+})
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac75eccf-2ec8-462c-822c-a6e6b0aecd23'
+};
