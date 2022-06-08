b49f297505101ebf393b7dea0881e8fb5319ac8b
fabrizioiacobucci <fabrizio.iacobucci90@mail.com>
2022-06-08 15:57:49 +0200

    mysql

--- config/database.js
+++ config/database.js
@@ -18,7 +18,7 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
-    connection: {
+    connection: { 
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi'),
