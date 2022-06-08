af3fd1f1388b26354cdf3b2094d57e0e909689ad
fabrizioiacobucci <fabrizio.iacobucci90@mail.com>
2022-06-08 12:31:31 +0200

    db

--- config/database.js
+++ config/database.js
@@ -4,12 +4,8 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
         useNullAsDefault: true,
--- config/plugins.js
+++ config/plugins.js
@@ -7,5 +7,12 @@ module.exports = ({ env }) => ({
             small: 500,
             xsmall: 64
         }
-    }
+    },
+        email: {
+                provider: 'sendmail',
+                settings: {
+                        defaultFrom: 'tatianastropkaiova@gmail.com',
+                        ddefaultReplyTo: 'tatianastropkaiova@gmail.com'
+                }
+        }
 })
